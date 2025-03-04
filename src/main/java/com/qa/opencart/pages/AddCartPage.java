93bde315f11098a8107bb57f2f476238d7ade2b2
unknown <smitachougale18@gmail.com>
2025-03-04 23:55:53 +0530

    updated code on the basis of Tushar's review

--- src/main/java/com/qa/opencart/pages/AddCartPage.java
+++ src/main/java/com/qa/opencart/pages/AddCartPage.java
@@ -1,8 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class AddCartPage {
 	
 	String string = "Test";
+	
+	By name= By.name("id");
+	
+	public AddCartPage() {
+		
+	}
 	
 	
 	
